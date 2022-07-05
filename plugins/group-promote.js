d1ba70749ed8cda8019942b20d71dde8e7656732
perkasalintang52 <98840516+perkasalintang52@users.noreply.github.com>
2022-07-05 21:44:39 +0700

    Update group-promote.js

--- plugins/group-promote.js
+++ plugins/group-promote.js
@@ -8,7 +8,7 @@ let handler = async (m, { teks, conn, isOwner, isAdmin, args }) => {
 if(m.quoted.sender === ownerGroup || m.quoted.sender === conn.user.jid) return;
 let usr = m.quoted.sender;
 let nenen = await conn.groupParticipantsUpdate(m.chat, [usr], "promote"); return;
-if (nenen) m.reply(`sukses promote @${user.split('@')[0]}!`);
+if (nenen) m.reply(`sukses naik jabatan @${user.split('@')[0]}!`);
 }
   if (!m.mentionedJid[0]) throw `tag yang mau dinaikan jabatannya`;
   let users = m.mentionedJid.filter(
